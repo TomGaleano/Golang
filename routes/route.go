958d3f713a786daf469be0a4554e0e713df261c4
Tomás Espinosa <tom.ga.es@gmail.com>
2023-12-27 18:08:03 -0500

    CORS enabled and working

--- routes/route.go
+++ routes/route.go
@@ -4,13 +4,20 @@ import (
 	"github.com/TomGaleano/Golang/controller"
 	"github.com/TomGaleano/Golang/middleware"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Setup(app *fiber.App) {
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // Allow all origins for development (restrict in production)
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 	app.Post("/api/register", controller.Register)
 	app.Post("/api/login", controller.Login)
 
 	app.Use(middleware.IsAuthenticated)
+
 	app.Post("/api/createpost", controller.CreatePost)
 	app.Get("/api/allpost", controller.AllPost)
 	app.Get("/api/detailpost/:id", controller.DetailPost)
